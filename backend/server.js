77f4a411f2bb3cca89325523bd8c60927f91df7d
EduardoMarmentini <eduardomarmentini.dev@gmail.com>
2024-11-21 20:58:47 -0300

    Criado metodos get para consultar dados, criado banco mongo na cloud gratuito e criado string de conexão

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,56 @@
-import expresss from "express"
+import express from "express"
 
-const app = expresss();
+function findPost(postID){
+    return posts.findIndex((post) => {
+        return post.id === Number(postID);
+    });
+};
+
+const posts = [
+    {
+      id: 1,
+      descript: "Um gatinho fofo tomando sol na janela.",
+      image: "https://placecats.com/millie/300/150",
+    },
+    {
+      id: 2,
+      descript: "Gato curioso explorando uma caixa de papelão.",
+      image: "https://placecats.com/millie/300/150",
+    },
+    {
+      id: 3,
+      descript: "Um felino elegante se espreguiçando no sofá.",
+      image: "https://placecats.com/millie/300/150",
+    },
+    {
+      id: 4,
+      descript: "Gato brincando com um novelo de lã.",
+      image: "https://placecats.com/millie/300/150",
+    },
+    {
+      id: 5,
+      descript: "Um olhar penetrante: gato observando o mundo com curiosidade.",
+      image: "https://placecats.com/millie/300/150",
+    }
+];
+
+const app = express();
+
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log("Server listening...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send(
-        "Welcome to immersion"
+app.get("/posts", (req, res) => {
+    res.status(200).json(
+        posts
+    );
+});
+
+
+app.get("/posts/:id", (req, res) => {
+    res.status(200).json(
+        posts[findPost(req.params.id)]
     );
 });
